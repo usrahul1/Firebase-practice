1bb398842e88910cdb7e69dad705ede788f217d1
Sai Rahul <sairahulurumu@gmail.com>
2025-05-02 20:46:24 +0530

    env file added

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -14,12 +14,12 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 const FirebaseContext = createContext(null);
 
 const firebaseConfig = {
-	apiKey: "AIzaSyC4BvkZ9ewxBnoH7wNeUfHsVOK0bdRpPoM",
-	authDomain: "bookify-60a5e.firebaseapp.com",
-	projectId: "bookify-60a5e",
-	storageBucket: "bookify-60a5e.firebasestorage.app",
-	messagingSenderId: "212436161771",
-	appId: "1:212436161771:web:42ac37f464eb0ab0265aae",
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
